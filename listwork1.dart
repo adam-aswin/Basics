a1d78d7876452c1d6a0a3d83bd2e5dd81042056f
sarathps33 <sarathps224@gmail.com>
2024-07-09 15:31:23 +0530

    new file

---
+++ listwork1.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+void main() {
+  File file = File("list.txt");
+  List ls = [];
+  Map<String, dynamic> mp = {};
+  String s = "";
+  while (true) {
+    print("====================");
+    print("1.Add employee");
+    print("2.Salary");
+    print("3.Designation");
+    print("4.Display");
+    print("5.Exit");
+    print("====================");
+    print("Enter your choice: ");
+    int choice = int.parse(stdin.readLineSync()!);
+    print("====================");
+    if (choice == 1) {
+      print("Enter Employee name:");
+      String? x = stdin.readLineSync();
+      mp["Name"] = x;
+    } else if (choice == 2) {
+      print("Enter Employee salary:");
+      double? y = double.parse(stdin.readLineSync()!);
+      mp["Salary"] = y;
+    } else if (choice == 3) {
+      print("Enter Employee designation:");
+      String? z = stdin.readLineSync();
+      mp["Designation"] = z;
+      s = mp.toString();
+      ls = s.split(",");
+      file.writeAsStringSync("\n$ls", mode: FileMode.append);
+    } else if (choice == 4) {
+      String c = file.readAsStringSync();
+      print(c);
+    } else if (choice == 5) {
+      print("Exiting...");
+      break;
+    } else {
+      print("Ivalid option");
+    }
+  }
+}
