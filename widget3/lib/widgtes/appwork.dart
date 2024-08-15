70b5d259843528a9206de690c3f3412a28f0a8d8
adam-aswin <aswinsachu2118@gmail.com>
2024-08-15 15:57:28 +0530

    filrs updated

--- widget3/lib/widgtes/appwork.dart
+++ widget3/lib/widgtes/appwork.dart
@@ -48,27 +48,30 @@ class AppWork extends StatelessWidget {
             ),
           ),
         ],
-        backgroundColor: Colors.grey[900],
+        backgroundColor: Colors.grey[850],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        unselectedLabelStyle: TextStyle(color: Colors.black),
+        showUnselectedLabels: true,
+        useLegacyColorScheme: false,
         items: [
           BottomNavigationBarItem(
-            
-              icon: Icon(
-                Icons.menu,
-                color: Colors.black,
-              ),
-              label: "manage",
-              
-              tooltip: "manage",
-              ),
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            label: "manage",
+            tooltip: "manage",
+          ),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.payment,
-                color: Colors.grey,
-              ),
-              label: "pay",
-              tooltip: "pay"),
+            icon: Icon(
+              Icons.payment,
+              color: Colors.grey,
+            ),
+            label: "pay",
+            tooltip: "pay",
+          ),
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.shopping_bag_outlined,
@@ -86,12 +89,15 @@ class AppWork extends StatelessWidget {
         ],
       ),
       body: Container(
-        color: Colors.grey[900],
+        color: Colors.grey[850],
         child: Container(
           margin: EdgeInsets.only(top: 10),
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
               color: const Color.fromARGB(255, 227, 231, 243)),
@@ -103,6 +109,7 @@ class AppWork extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Container(
+                      margin: EdgeInsets.only(top: 20),
                       padding: EdgeInsets.all(20),
                       width: MediaQuery.of(context).size.width * .43,
                       height: 250,
@@ -163,6 +170,7 @@ class AppWork extends StatelessWidget {
                     Container(
                       width: MediaQuery.of(context).size.width * .43,
                       height: 250,
+                      margin: EdgeInsets.only(top: 20),
                       padding: EdgeInsets.all(20),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
@@ -250,12 +258,242 @@ class AppWork extends StatelessWidget {
                 height: 20,
               ),
               Container(
-                height: 200,
+                height: 190,
                 width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.all(19),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   color: Colors.white,
                 ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "EXPLORE AIRTEL BESTSELLERS",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey[600],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 65,
+                                width: 65,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromARGB(255, 227, 231, 243),
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Icon(Icons.router_rounded),
+                              ),
+                              Text(
+                                "Wi-Fi",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          padding: EdgeInsets.only(top: 15),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 65,
+                                width: 65,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromARGB(255, 227, 231, 243),
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Icon(Icons.sim_card_rounded),
+                              ),
+                              Text(
+                                "upgrade to",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                "postpaid",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 65,
+                                width: 65,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromARGB(255, 227, 231, 243),
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Icon(Icons.settings_input_antenna),
+                              ),
+                              Text(
+                                "DTH",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 65,
+                                width: 65,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromARGB(255, 227, 231, 243),
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Icon(Icons.sim_card_download),
+                              ),
+                              Text(
+                                "new prepaid SIM",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    height: 100,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          child: Image.asset(
+                            "./lib/icons/phone.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Text(
+                          "call",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          "manager",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    height: 100,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          child: Image.asset(
+                            "./lib/icons/phone.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Text(
+                          "rewards &",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          "coupons",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    height: 100,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          child: Image.asset(
+                            "./lib/icons/phone.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Text(
+                          "refer & get",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          "₹ 300",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               )
             ],
           ),
