586686cafcc72fdaee06fe4a0f432310b8877497
adam-aswin <aswinsachu2118@gmail.com>
2024-09-06 15:58:21 +0530

    updated

--- bloodbank/lib/pages/Addpage.dart
+++ bloodbank/lib/pages/Addpage.dart
@@ -8,13 +8,28 @@ class Addpage extends StatefulWidget {
 }
 
 class _AddpageState extends State<Addpage> {
+  String? _selectedblood;
+  final List<String> bloodgroup = [
+    "A-",
+    "A+",
+    "AB-",
+    "AB+",
+    "O+",
+    "O-",
+    "B+",
+    "B-",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 247, 230, 230),
         appBar: AppBar(
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
           backgroundColor: const Color.fromARGB(255, 168, 12, 0),
           title: Text(
-            "Create Profile",
+            "Donor Details",
             style: TextStyle(color: Colors.white),
           ),
           centerTitle: true,
@@ -82,16 +97,230 @@ class _AddpageState extends State<Addpage> {
             SizedBox(
               height: 90,
             ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 50,
-              margin: EdgeInsets.only(left: 30, right: 30),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: const Color.fromARGB(255, 247, 174, 174)),
-            )
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      top: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Name",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * .7,
+                          height: 50,
+                          padding: EdgeInsets.all(10),
+                          // margin: EdgeInsets.only(left: 30, right: 30),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: const Color.fromARGB(255, 247, 174, 174),
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Name",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      top: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Age",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * .7,
+                          height: 50,
+                          padding: EdgeInsets.all(10),
+                          // margin: EdgeInsets.only(left: 30, right: 30),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: const Color.fromARGB(255, 247, 174, 174),
+                          ),
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Age",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      top: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Email",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * .7,
+                          height: 50,
+                          padding: EdgeInsets.all(10),
+                          // margin: EdgeInsets.only(left: 30, right: 30),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: const Color.fromARGB(255, 247, 174, 174),
+                          ),
+                          child: TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Email",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      top: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Number",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * .7,
+                          height: 50,
+                          padding: EdgeInsets.all(10),
+                          // margin: EdgeInsets.only(left: 30, right: 30),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: const Color.fromARGB(255, 247, 174, 174),
+                          ),
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Mobile Number",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      top: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Blood Group",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * .22,
+                          height: 50,
+                          padding: EdgeInsets.only(left: 8),
+                          // margin: EdgeInsets.only(left: 30, right: 30),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: const Color.fromARGB(255, 247, 174, 174),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(_selectedblood == null
+                                  ? "Select a Blood Group"
+                                  : "$_selectedblood"),
+                              DropdownButton(
+                                icon:
+                                    Icon(Icons.arrow_drop_down_circle_outlined),
+                                dropdownColor:
+                                    const Color.fromARGB(255, 247, 230, 230),
+                                underline: Container(
+                                  height: 0,
+                                ),
+                                items: bloodgroup.map(
+                                  (String blood) {
+                                    return DropdownMenuItem(
+                                      value: blood,
+                                      child: Text(blood),
+                                    );
+                                  },
+                                ).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    _selectedblood = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ));
-    ;
   }
 }
--- bloodbank/lib/pages/homescreen.dart
+++ bloodbank/lib/pages/homescreen.dart
@@ -29,15 +29,20 @@ class _HomescreenState extends State<Homescreen> {
       body: Container(
         padding: EdgeInsets.all(15),
         width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
+        // height: MediaQuery.of(context).size.height,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              padding: EdgeInsets.all(20),
+              padding: EdgeInsets.all(15),
               width: MediaQuery.of(context).size.width,
               height: 200,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 168, 12, 0),
+                  width: 2,
+                ),
                 color: Colors.white,
               ),
               child: Row(
@@ -93,14 +98,7 @@ class _HomescreenState extends State<Homescreen> {
                 Container(
                   width: MediaQuery.of(context).size.width * .45,
                   height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * .45,
-                  height: 120,
+                  padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       color: Colors.white,
@@ -117,26 +115,99 @@ class _HomescreenState extends State<Homescreen> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Container(
-                        height: 80,
-                        width: 80,
-                        child: Image.asset("./images/add.webp"),
+                        height: 70,
+                        width: 70,
+                        child: Image.asset("./images/request.webp"),
                       ),
                       SizedBox(
                         height: 5,
                       ),
                       Container(
-                          height: 30,
-                          width: 80,
                           child: Text(
-                            "Add Donor",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ))
+                        "Request For Blood",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          // fontSize: 10,
+                        ),
+                      ))
                     ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/add");
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * .45,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 5,
+                            spreadRadius: 1,
+                            color: Colors.black26,
+                            offset: Offset(5, 5),
+                          )
+                        ]),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 80,
+                          width: 80,
+                          child: Image.asset("./images/add.webp"),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                            height: 30,
+                            width: 80,
+                            child: Text(
+                              "Add Donor",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ))
+                      ],
+                    ),
                   ),
                 )
               ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Donors",
+              style: TextStyle(
+                // decoration: TextDecoration.underline,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 5,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width * .8,
+                      height: 100,
+                      margin: EdgeInsets.only(
+                        top: 10,
+                        right: 10,
+                        bottom: 70,
+                      ),
+                      color: Colors.white,
+                    );
+                  }),
             )
           ],
         ),
